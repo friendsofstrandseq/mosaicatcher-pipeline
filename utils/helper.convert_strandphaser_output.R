a5c194ff201ed4c488def2271d676d67f090f631
Sascha Meiers <meiers@embl.de>
2017-10-26 16:02:45 +0200

    Fix for prev. commit

--- utils/helper.convert_strandphaser_output.R
+++ utils/helper.convert_strandphaser_output.R
@@ -1,8 +1,8 @@
 library(data.table);
 library(assertthat)
-d = fread(snakemake@input[["phased_states"]])
-e = fread(snakemake@input[["info"]])
-g = fread(snakemake@intput[["initial_states"]])
+e = fread(snakemake@input[["phased_states"]])
+d = fread(snakemake@input[["info"]])
+g = fread(snakemake@input[["initial_states"]])
 
 d$bam = basename(d$bam);
 
@@ -14,10 +14,11 @@ f = merge(d, e, by = "bam")[, .(chrom,start,end,sample,cell,class)]
 # Note that there is still a bug in Venla's strand state detection.
 g = merge(g,f, by = c("chrom","start","end","sample","cell"), all.x = T)
 
+
 # Overwrite with David's phased strand state if available!
 g = g[, class := ifelse(!is.na(class.y), class.y, class.x)][]
 g$class.x = NULL
 g$class.y = NULL
 g = g[, .(chrom, start, end, sample, cell, class)]
 
-write.table(g, file=snakemake@output[[1]], quote=F, row.names=F, col.names=F, sep="\t")
+write.table(g, file=snakemake@output[[1]], quote=F, row.names=F, col.names=T, sep="\t")
