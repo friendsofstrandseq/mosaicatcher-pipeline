1fb88c7f751972e8a97043a6bb271ce7326125d6
daewoooo <daewoooo@gmail.com>
2017-11-14 10:11:47 +0100

    Added StrandPhaseR pipeline

---
+++ StrandPhaseR_pipeline.R
@@ -0,0 +1,11 @@
+#!/usr/bin/Rscript
+
+args=commandArgs(TRUE)
+
+#add user defined path to load needed libraries
+.libPaths( c( .libPaths(), args[6]) )
+
+suppressPackageStartupMessages(library(StrandPhaseR))
+suppressPackageStartupMessages(library(BSgenome.Hsapiens.UCSC.hg19))
+
+strandPhaseR(inputfolder=args[1], outputfolder=args[2], configfile = args[3],  WCregions = args[4] , positions=args[5])
