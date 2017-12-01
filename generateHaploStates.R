9865fd33e0bfac68ff5199ac54c44768c7f63853
maryamghr <maryam.ghareghani@gmail.com>
2017-12-01 10:11:27 +0100

    Update generateHaploStates.R

--- generateHaploStates.R
+++ generateHaploStates.R
@@ -79,7 +79,7 @@ allStatus = function(n,m)
 decodeStatus = function(binaryStatus)
 {
   status = NULL
-  pos = str_locate_all(binaryStatus, "0")[[1]][,1]
+  pos = stringr::str_locate_all(binaryStatus, "0")[[1]][,1]
   pos = c(0, pos, nchar(binaryStatus)+1)
   
   for (i in 1:length(pos)-1)
