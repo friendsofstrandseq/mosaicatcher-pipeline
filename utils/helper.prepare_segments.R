ef735d68ee1a5b2f920e95ab7f94e34ea6babafb
Sascha Meiers <meiers@embl.de>
2017-12-13 09:02:52 +0100

    More LOG messages

--- utils/helper.prepare_segments.R
+++ utils/helper.prepare_segments.R
@@ -1,6 +1,10 @@
+sink(snakemake@log[[1]])
 library(data.table)
 qu = snakemake@params[["quantile"]]
+qu
 d = fread(snakemake@input[[1]])
+d
 # type = 1 is important to get discrete values!
 d = d[, .SD[k == quantile(1:max(k), qu, type = 1)], by=chrom][,.(k, chrom, bps = breakpoint)]
+d
 write.table(d, file = snakemake@output[[1]], row.names=F, quote=F, sep="\t")
