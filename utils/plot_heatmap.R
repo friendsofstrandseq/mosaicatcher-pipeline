ae175c055636289fe8a1dad1309b4c1b5617dc13
maryamghr <maryam.ghareghani@gmail.com>
2018-03-27 10:38:41 +0200

    add GT classes

--- utils/plot_heatmap.R
+++ utils/plot_heatmap.R
@@ -12,9 +12,13 @@ GTprobsFile = snakemake@input[["genotypeProbs"]]
 GTprobs <- read.table(GTprobsFile, stringsAsFactors = F, header = T)
 hapProbs <- read.table(hapProbsFile, stringsAsFactors = F, header = T)
 
+# Converting the prob tables to GT classe
+GTclassProbs <- get_GT_class_prob_table(GTprobs)
+hapClassProbs <- get_GT_class_prob_table(GTprobs)
+
 # getting the splitted prob tables including the jump probs
-GTprobs.l.chrom <- addJumpProbs(GTprobs)
-hapProbs.l.chrom <- addJumpProbs(hapProbs)
+GTprobs.l.chrom <- addJumpProbs(GTclassProbs)
+hapProbs.l.chrom <- addJumpProbs(hapClassProbs)
 
 pdf(snakemake@output[[1]])
 for (k in 1:length(GTprobs.l.chrom))
