8347e44cb1364c93dd5c4f934dc018944e504a2e
Sascha Meiers <meiers@embl.de>
2018-05-16 15:53:25 +0200

    Finished first part of MosaiClassifier. Preparing for the NB part

--- utils/mosaiClassifier.snakemake.R
+++ utils/mosaiClassifier.snakemake.R
@@ -4,10 +4,10 @@ library(pheatmap)
 source("utils/mosaiClassifier/mosaiClassifier.R")
 
 
-# counts = fread(paste("zcat","counts/simulation0-50000/50000_fixed.txt.gz"))
-# info   = fread("counts/simulation0-50000/50000_fixed.info")
-# strand = fread("strand_states/simulation0-50000/final.txt")
-# segs   = fread("segmentation2/simulation0-50000/50000_fixed.medium.txt") 
+#counts = fread(paste("zcat","counts/simulation0-50000/50000_fixed.txt.gz"))
+#info   = fread("counts/simulation0-50000/50000_fixed.info")
+#strand = fread("strand_states/simulation0-50000/final.txt")
+#segs   = fread("segmentation2/simulation0-50000/50000_fixed.medium.txt")
 
 
 # Currently read files from the Snakemake pipeline
@@ -17,7 +17,8 @@ strand = fread(snakemake@input[["states"]])
 segs   = fread(snakemake@input[["bp"]])
 
 
-d = getSVProbabilities(counts, info, strand, segs)
+d = mosaiClassifierPrepare(counts, info, strand, segs)
+d = mosaiClassifierCalcProbs(d)
 saveRDS(d, file = snakemake@output[[1]])
 
 
