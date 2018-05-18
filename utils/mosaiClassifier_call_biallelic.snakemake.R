1b4bd2472cd825e46a6ba7a65f9e2abba603e33c
Sascha Meiers <meiers@embl.de>
2018-05-18 14:42:08 +0200

    added rules for biallelic caller

---
+++ utils/mosaiClassifier_call_biallelic.snakemake.R
@@ -0,0 +1,11 @@
+library(data.table)
+source("utils/mosaiClassifier/makeSVcalls.R")
+
+probs = readRDS(snakemake@input[["probs"]])
+llr   = as.numeric(snakemake@params[["llr"]])
+
+probs <- mosaiClassifierPostProcessing(probs)
+probs <- forceBiallelic(probs)
+tab <- makeSVCallSimple(probs)
+
+write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)
