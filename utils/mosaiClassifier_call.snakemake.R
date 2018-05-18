6fef21c89f15dedafee5935bbedad8f460569e64
Sascha Meiers <meiers@embl.de>
2018-05-18 14:45:37 +0200

    Make the post processing before calling SVs

--- utils/mosaiClassifier_call.snakemake.R
+++ utils/mosaiClassifier_call.snakemake.R
@@ -4,6 +4,7 @@ source("utils/mosaiClassifier/makeSVcalls.R")
 probs = readRDS(snakemake@input[["probs"]])
 llr   = as.numeric(snakemake@params[["llr"]])
 
+probs <- mosaiClassifierPostProcessing(probs)
 tab <- makeSVCallSimple(probs)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)
