1b6bfdac158e6e1097369383773a6021b318eef3
Sascha Meiers <meiers@embl.de>
2018-06-04 15:10:32 +0200

    Include log-likelihood-ratio filter and normalization

--- utils/mosaiClassifier_call_biallelic.snakemake.R
+++ utils/mosaiClassifier_call_biallelic.snakemake.R
@@ -3,10 +3,10 @@ library(data.table)
 source("utils/mosaiClassifier/makeSVcalls.R")
 
 probs = readRDS(snakemake@input[["probs"]])
-llr   = as.numeric(snakemake@params[["llr"]])
+llr   = as.numeric(snakemake@wildcards[["llr"]])
 
 probs <- mosaiClassifierPostProcessing(probs)
 probs <- forceBiallelic(probs)
-tab <- makeSVCallSimple(probs)
+tab <- makeSVCallSimple(probs, llr_thr = llr)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)
