59d37393fc95ca64770176ad53ba013ac0f50356
Sascha Meiers <meiers@embl.de>
2018-06-12 20:50:20 +0200

    Enable new segmentation

--- utils/helper.prepare_segments.R
+++ utils/helper.prepare_segments.R
@@ -5,6 +5,6 @@ qu
 d = fread(snakemake@input[[1]])
 d
 # type = 1 is important to get discrete values!
-d = d[, .SD[k == quantile(1:max(k), qu, type = 1)], by=chrom][,.(k, chrom, bps = breakpoint)]
+d = d[, .SD[k == quantile(1:max(k), qu, type = 1)], by=chrom][,.(k, chrom, bps)]
 d
 write.table(d, file = snakemake@output[[1]], row.names=F, quote=F, sep="\t")
