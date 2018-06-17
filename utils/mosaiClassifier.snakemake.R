2763c588b46ba27f4c09b492cd4256a28abf3122
Sascha Meiers <meiers@embl.de>
2018-06-17 10:52:27 +0200

    Deprecated warning for old normalization

--- utils/mosaiClassifier.snakemake.R
+++ utils/mosaiClassifier.snakemake.R
@@ -11,10 +11,12 @@ strand = fread(snakemake@input[["states"]])
 segs   = fread(snakemake@input[["bp"]])
 
 
+# DEPERECATED: this version of normalization is no longer used
 # is there a normalization file given?
 if ("norm" %in% names(snakemake@input) && length(snakemake@input[["norm"]])>0) {
   message("[MosaiClassifier] Read normalization from ", snakemake@input[["norm"]])
   normalization = fread(snakemake@input[["norm"]])
+  message("[Warning] Normalization file specified, but this option is no longer available")
 } else {
   normalization = NULL
 }
@@ -26,7 +28,7 @@ if ("CW" %in% strand$class) {
   haplotypeMode = F
 }
 
-d = mosaiClassifierPrepare(counts, info, strand, segs, normalization)
+d = mosaiClassifierPrepare(counts, info, strand, segs)
 e = mosaiClassifierCalcProbs(d, maximumCN = 4, haplotypeMode = haplotypeMode)
 
 saveRDS(e, file = snakemake@output[[1]])
