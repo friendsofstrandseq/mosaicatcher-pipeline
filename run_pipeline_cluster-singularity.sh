d47c1d9d12591bb62399c5dbb3d1e0bb68985b52
Sascha Meiers <meiers@embl.de>
2018-06-21 22:11:14 +0200

    Fix for miscommunication problem between SLURM and snakemake by Jelle Scholtalbers

--- run_pipeline_cluster-singularity.sh
+++ run_pipeline_cluster-singularity.sh
@@ -4,6 +4,8 @@
 REF="/g/korbel/shared/datasets/refgenomes/human/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
 R_REF="/g/korbel/meiers/R-lib/3.4.0-foss-2016b/BSgenome.Hsapiens.UCSC.hg38/extdata/single_sequences.2bit"
 
+mkdir slurm 2> /dev/null
+
 snakemake \
     -j 100 \
     --configfile Snake.config-singularity.json \
@@ -13,7 +15,8 @@ snakemake \
                         -B ${R_REF}:/usr/local/lib/R/site-library/BSgenome.Hsapiens.UCSC.hg38/extdata/single_sequences.2bit:ro" \
 	--cluster-config cluster.json \
     --local-cores 8 \
-    --cluster "sbatch -o slurm/{rule}.%j.log -e slurm/{rule}.%j.log --cpus-per-task {cluster.n} --time {cluster.time} --mem {cluster.mem}" \
+    --cluster "sbatch --parsable -o slurm/{rule}.%j.log -e slurm/{rule}.%j.log --cpus-per-task {cluster.n} --time {cluster.time} --mem {cluster.mem}" \
+    --cluster-status ./cluster_status.py \
     --latency-wait 60 \
     --timestamp \
     --keep-going \
