9e9d8261e493e87d489b30cd27dff83e0337657c
Tobias Marschall <tobias.marschall@0ohm.net>
2018-07-04 13:06:02 +0200

    Make calls with/without population priors and with different regularization factors

--- utils/mosaiClassifier_call.snakemake.R
+++ utils/mosaiClassifier_call.snakemake.R
@@ -1,10 +1,16 @@
+log <- file(snakemake@log[[1]], open='wt')
+sink(file=log, type='message')
+sink(file=log, type='output')
+
 library(data.table)
 source("utils/mosaiClassifier/makeSVcalls.R")
 
-probs = readRDS(snakemake@input[["probs"]])
-llr   = as.numeric(snakemake@wildcards[["llr"]])
+probs                = readRDS(snakemake@input[["probs"]])
+llr                  = as.numeric(snakemake@wildcards[["llr"]])
+use.pop.priors       = eval(parse(text=snakemake@wildcards[["pop_priors"]]))
+regularizationFactor = 10^(-as.numeric(snakemake@wildcards[["regfactor"]]))
 
-probs <- mosaiClassifierPostProcessing(probs)
-tab <- makeSVCallSimple(probs, llr_thr = llr)
+probs <- mosaiClassifierPostProcessing(probs, regularizationFactor = regularizationFactor)
+tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)
