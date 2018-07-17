e38e02d9fbca35b977bb0cc1a085d012ce6bc528
Tobias Marschall <tobias.marschall@0ohm.net>
2018-07-17 21:16:33 +0200

    Fix off by one error (quick and dirty)

--- utils/haplotagProbs.snakemake.R
+++ utils/haplotagProbs.snakemake.R
@@ -7,6 +7,9 @@ source('utils/haplotagProbs.R')
 haplotagCounts <- fread(snakemake@input[["haplotag_table"]])
 probs <- readRDS(snakemake@input[["sv_probs_table"]])
 
+# FIXME: quick and dirty fix for off by one start coordinates of segments
+haplotagCounts[,start:=start-1]
+
 probs <- addHaploCountProbs(probs, haplotagCounts, alpha=0.05)
 
 saveRDS(probs, file=snakemake@output[[1]])
