052e5afdd85ea8553c79427066d6d3cdb5b66af7
Tobias Marschall <tobias.marschall@0ohm.net>
2018-07-17 21:17:52 +0200

    Apply haplotag probabilities during SV calling (optionally)

--- utils/mosaiClassifier_call.snakemake.R
+++ utils/mosaiClassifier_call.snakemake.R
@@ -8,9 +8,10 @@ source("utils/mosaiClassifier/makeSVcalls.R")
 probs                = readRDS(snakemake@input[["probs"]])
 llr                  = as.numeric(snakemake@wildcards[["llr"]])
 use.pop.priors       = eval(parse(text=snakemake@wildcards[["pop_priors"]]))
+use.haplotags        = eval(parse(text=snakemake@wildcards[["use_haplotags"]]))
 regularizationFactor = 10^(-as.numeric(snakemake@wildcards[["regfactor"]]))
 
 probs <- mosaiClassifierPostProcessing(probs, regularizationFactor = regularizationFactor)
-tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors)
+tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors, use.haplotags = use.haplotags)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)
