friendsofstrandseq/pipeline

added a cutoff on the fraction of blacklisted bins for SV calling

utils/mosaiClassifier_call.snakemake.R
@@ -11,8 +11,10 @@ use.pop.priors       = eval(parse(text=snakemake@wildcards[["pop_priors"]]))
 use.haplotags        = eval(parse(text=snakemake@wildcards[["use_haplotags"]]))
 regularizationFactor = 10^(-as.numeric(snakemake@wildcards[["regfactor"]]))
 genotype.cutoff      = as.numeric(snakemake@wildcards[["gtcutoff"]])
+minFrac.used.bins    = as.numeric(snakemake@params[["minFrac_used_bins"]])
+bin.size     	     = as.numeric(snakemake@wildcards[["windows"]])
 
 probs <- mosaiClassifierPostProcessing(probs, regularizationFactor = regularizationFactor)
-tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors, use.haplotags = use.haplotags, genotype.cutoff = genotype.cutoff)
+tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors, use.haplotags = use.haplotags, genotype.cutoff = genotype.cutoff, bin.size, minFrac.used.bins = minFrac.used.bins)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)

friendsofstrandseq/mosaicatcher-pipeline

utils/mosaiClassifier_call.snakemake.R
@@ -11,8 +11,10 @@ use.pop.priors       = eval(parse(text=snakemake@wildcards[["pop_priors"]]))
 use.haplotags        = eval(parse(text=snakemake@wildcards[["use_haplotags"]]))
 regularizationFactor = 10^(-as.numeric(snakemake@wildcards[["regfactor"]]))
 genotype.cutoff      = as.numeric(snakemake@wildcards[["gtcutoff"]])
+minFrac.used.bins    = as.numeric(snakemake@params[["minFrac_used_bins"]])
+bin.size     	     = as.numeric(snakemake@wildcards[["window"]])
 
 probs <- mosaiClassifierPostProcessing(probs, regularizationFactor = regularizationFactor)
-tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors, use.haplotags = use.haplotags, genotype.cutoff = genotype.cutoff)
+tab <- makeSVCallSimple(probs, llr_thr = llr, use.pop.priors = use.pop.priors, use.haplotags = use.haplotags, genotype.cutoff = genotype.cutoff, bin.size, minFrac.used.bins = minFrac.used.bins)
 
 write.table(tab, file = snakemake@output[[1]], sep = "\t", quote=F, row.names = F, col.names = T)