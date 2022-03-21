31af9bf0b8fc8374d6416abc132392b55a988f30
weber <thomas.weber@etu.unistra.fr>
2022-03-21 08:49:47 +0100

    Cleaning to make it fct

--- run_pipeline_singularity.sh
+++ run_pipeline_singularity.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set these two paths to link large external data (reference genomes) to the respective places within the image
-REF="/g/korbel/shared/datasets/refgenomes/human/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
-R_REF="/g/korbel/meiers/R-lib/3.4.0-foss-2016b/BSgenome.Hsapiens.UCSC.hg38/extdata/single_sequences.2bit"
+REF="/gstock/biolo_datasets/variation/benchmark/Databases/RefSeq/ncbi-genomes-2022-02-17/GCF_000001405.39_GRCh38.p13_genomic.fna.gz"
+R_REF="/home/weber/.conda/envs/strandseqnation/lib/R/library/BSgenome.Hsapiens.UCSC.hg38/extdata/single_sequences.2bit"
 
 snakemake \
     -j 6 \
@@ -13,4 +13,6 @@ snakemake \
                         -B ${R_REF}:/usr/local/lib/R/site-library/BSgenome.Hsapiens.UCSC.hg38/extdata/single_sequences.2bit:ro" \
     --latency-wait 60 \
     --printshellcmd
+
+
 
