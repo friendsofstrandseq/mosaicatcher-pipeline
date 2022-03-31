2ff31ce9b32f61fbfc0379a04d55b9794bea4a2e
Thomas Weber <thomas.weber@embl.de>
2022-03-31 09:38:05 +0200

    Daily progress ; cleaning & docs

--- utils/install_strandphaser.R
+++ utils/install_strandphaser.R
@@ -1,4 +1,15 @@
-#!/usr/bin/env Rscript
-Sys.setenv(Renv='PWD')
+## #!/usr/bin/env Rscript
+# Sys.setenv(Renv='PWD')
+
+# Rscript version updated according to README 30-Mar-22 commit 69c9fb4
+# Long time execution : ~20 min
+
+install.packages("devtools", repos = "http://cran.us.r-project.org") 
 library(devtools)
-withr::with_libpaths(new = "utils/R-packages", install_git("git://github.com/daewoooo/StrandPhaseR.git", branch = "master"), "prefix")
+if (!requireNamespace("BiocManager", quietly=TRUE))
+    install.packages("BiocManager", repos = "http://cran.us.r-project.org") 
+BiocManager::install("GenomicRanges") 
+BiocManager::install("GenomicAlignments") 
+# withr::with_libpaths(new = "utils/R-packages", install_git("git://github.com/daewoooo/StrandPhaseR.git", branch = "master"), "prefix")
+install_github("daewoooo/StrandPhaseR")
+
