41e3a4711bf7112d2bc76dcb4cf333affeff95eb
Thomas Weber <thomas.weber@embl.de>
2022-04-04 22:55:06 +0200

    Daily progress ; cleaning & docs ; Functionnal until MosaiClassifier

--- utils/mosaiClassifier.snakemake.R
+++ utils/mosaiClassifier.snakemake.R
@@ -5,33 +5,20 @@ source("utils/mosaiClassifier/mosaiClassifier.R")
 
 
 # Currently read files from the Snakemake pipeline
-counts = fread(paste("zcat",snakemake@input[["counts"]]))
-info   = fread(snakemake@input[["info"]])
-strand = fread(snakemake@input[["states"]])
-segs   = fread(snakemake@input[["bp"]])
+counts <- fread(paste("zcat", snakemake@input[["counts"]]))
+info <- fread(snakemake@input[["info"]])
+strand <- fread(snakemake@input[["states"]])
+segs <- fread(snakemake@input[["bp"]])
 
-
-# DEPERECATED: this version of normalization is no longer used
-# is there a normalization file given?
-if ("norm" %in% names(snakemake@input) && length(snakemake@input[["norm"]])>0) {
-  message("[MosaiClassifier] Read normalization from ", snakemake@input[["norm"]])
-  normalization = fread(snakemake@input[["norm"]])
-  message("[Warning] Normalization file specified, but this option is no longer available")
-} else {
-  normalization = NULL
-}
 
 # haplotypeMode?
 if ("CW" %in% strand$class) {
-  haplotypeMode = T
+  haplotypeMode <- T
 } else {
-  haplotypeMode = F
+  haplotypeMode <- F
 }
 
-d = mosaiClassifierPrepare(counts, info, strand, segs)
-e = mosaiClassifierCalcProbs(d, maximumCN = 4, haplotypeMode = haplotypeMode)
+d <- mosaiClassifierPrepare(counts, info, strand, segs)
+e <- mosaiClassifierCalcProbs(d, maximumCN = 4, haplotypeMode = haplotypeMode)
 
 saveRDS(e, file = snakemake@output[[1]])
-
-
-
