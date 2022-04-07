ebdf20242cacfe7021f82f91f8a43f624ebe05f9
Thomas Weber <thomas.weber@embl.de>
2022-04-07 11:21:49 +0200

    Daily progress ; cleaning & docs ; Test to work with BAM specific to one chr, still not fully functionnal ; Classic usage functionnal until rule mosaiClassifier_calc_probs

--- utils/StrandPhaseR_pipeline.R
+++ utils/StrandPhaseR_pipeline.R
@@ -1,10 +1,14 @@
 #!/usr/bin/Rscript
+options(error = traceback)
+args <- commandArgs(TRUE)
 
-args=commandArgs(TRUE)
+# add user defined path to load needed libraries
+.libPaths(c(.libPaths(), args[6]))
 
-#add user defined path to load needed libraries
-.libPaths( c( .libPaths(), args[6]) )
+# suppressPackageStartupMessages(library(StrandPhaseR))
+library(devtools)
 
-suppressPackageStartupMessages(library(StrandPhaseR))
+# load package w/o installing
+load_all("/g/korbel2/weber/Gits/StrandPhaseR")
 
-strandPhaseR(inputfolder=args[1], outputfolder=args[2], configfile = args[3],  WCregions = args[4], positions=args[5], fillMissAllele=args[5])
+strandPhaseR(inputfolder = args[1], outputfolder = args[2], configfile = args[3], WCregions = args[4], positions = args[5], fillMissAllele = args[5])
