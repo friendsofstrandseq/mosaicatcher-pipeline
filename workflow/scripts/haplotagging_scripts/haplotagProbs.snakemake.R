b177862e7e629c8704a3a780e119862a819992e8
Thomas Weber <thomas.weber@embl.de>
2022-04-20 18:48:49 +0200

    Split into files ; define modes & associated conda envs ; web report working ; run on a single chromosome by keeping full initial BAM  ; functionnal until haplotagging

--- workflow/scripts/haplotagging_scripts/haplotagProbs.snakemake.R
+++ workflow/scripts/haplotagging_scripts/haplotagProbs.snakemake.R
@@ -2,7 +2,7 @@ log <- file(snakemake@log[[1]], open = "wt")
 sink(file = log, type = "message")
 sink(file = log, type = "output")
 
-source("scripts/haplotagging_scripts/haplotagProbs.R")
+source("utils/haplotagProbs.R")
 
 haplotagCounts <- fread(snakemake@input[["haplotag_table"]])
 probs <- readRDS(snakemake@input[["sv_probs_table"]])
