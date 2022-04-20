c65efcf200bc49e14be16977ee5fc2637c1245e1
Thomas Weber <thomas.weber@embl.de>
2022-04-20 18:48:49 +0200

    Split into files ; define modes & associated conda envs ; web report working ; run on a single chromosome by keeping full initial BAM  ; functionnal until haplotagging

---
+++ workflow/scripts/haplotagging_scripts/haplotagTable.snakemake.R
@@ -0,0 +1,12 @@
+log <- file(snakemake@log[[1]], open = "wt")
+sink(file = log, type = "message")
+sink(file = log, type = "output")
+
+system("LC_MEASUREMENT=C")
+
+source("scripts/haplotagging_scripts/haplotagTable.R")
+
+
+tab <- getHaplotagTable2(bedFile = snakemake@input[["bed"]], bam.file = snakemake@input[["bam"]], file.destination = snakemake@output[["tsv"]])
+
+# SVplotting(snakemake@input[["sv_calls"]], snakemake@output[["barplot_high"]], snakemake@output[["barplot_med"]], snakemake@output[["barplot_low"]], snakemake@output[["barplot_rare"]])
