952bf0c8cdd626ce31ebd7c36fc651045e65dff3
Thomas Weber <thomas.weber@embl.de>
2022-05-03 11:28:37 +0200

    Cleaning

--- utils/install_maryam.R
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env Rscript
-Sys.setenv(Renv='PWD')
-library(devtools)
-withr::with_libpaths(new = "utils/R-packages2", install_git("git://github.com/friendsofstrandseq/MaRyam.git", branch = "master"), "prefix")
