564c1cec5b0309a2b4d2787dbfc7988c69456e62
Thomas Weber <thomas.weber@embl.de>
2022-06-21 09:20:11 +0200

    Solve StrandPhaseR docker image installation (minor)

--- afac/update_tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-OLD_TAG="$1" &&
-    NEW_TAG=$(echo "$OLD_TAG" | sed 's/v//g') &&
-    echo "$OLD_TAG" "$NEW_TAG" &&
-    git tag "$NEW_TAG" "$OLD_TAG" &&
-    git tag -d "$OLD_TAG" &&
-    git push $2 "$NEW_TAG" :"$OLD_TAG"
--- workflow/scripts/segmentation_scripts/fix_segmentation.awk
+++
@@ -1,8 +0,0 @@
-# FIX SEGMENTATION
-BEGIN {OFS="\t"} 
-    {
-        if ($1 == name) { 
-            $12 = int(($14-1)/int(window)); 
-            }
-        print
-    }
