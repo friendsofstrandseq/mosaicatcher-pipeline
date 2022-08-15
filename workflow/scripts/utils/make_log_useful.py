0e807c69f906809c8a7e9eaed94a3a996a518446
Thomas Weber <thomas.weber@embl.de>
2022-08-15 12:57:19 +0200

    Major update: * Filter_bad_cells correction (ashleys predictions intersection with mosaic info file) * From detection of sexual chromosomes to detection of haploidy for any chrom (based on peter & tobias script) + plot * Haploidy used for checkpoint rule in order to bypass freebayes + strandphaser * Plot issues correction (clustering) * Filenames modification

---
+++ workflow/scripts/utils/make_log_useful.py
@@ -0,0 +1,45 @@
+import sys, os
+
+
+def make_log_useful(log_path, status):
+
+    error_buffer = []
+    record = 0
+    with open(log_path, "r") as logfile:
+        for line in logfile:
+            if "error" in line.lower() or "exception" in line.lower():
+                if record == 0:
+                    error_buffer.append("=== Recording ERROR entry")
+                record = 10
+                error_buffer.append(line.strip())
+            elif line.lower().startswith("[w::") or line.lower().startswith("[e::"):
+                if record == 0:
+                    error_buffer.append("=== Recording library stderr")
+                record = 3
+                error_buffer.append(line.strip())
+            elif record > 0:
+                error_buffer.append(line.strip())
+                record -= 1
+                if record == 0:
+                    error_buffer.append("=== STOP recording error entry")
+            else:
+                continue
+
+    with open(log_path_out, "w") as logfile:
+        _ = logfile.write("\n".join(error_buffer))
+        _ = logfile.write("\n\n")
+
+    my_env = dict(os.environ)
+    with open(log_path, "a") as logfile:
+        _ = logfile.write("\n===[{}]===\n".format(status))
+        _ = logfile.write("Host: {}\n".format(my_env.get("HOST", "N/A")))
+        _ = logfile.write("Hostname: {}\n".format(my_env.get("HOSTNAME", "N/A")))
+        _ = logfile.write("Display: {}\n".format(my_env.get("DISPLAY", "N/A")))
+        _ = logfile.write("Shell: {}\n".format(my_env.get("SHELL", "N/A")))
+        _ = logfile.write("Terminal: {}\n".format(my_env.get("TERM", "N/A")))
+        _ = logfile.write("Screen: {}\n".format(my_env.get("STY", "N/A")))
+        _ = logfile.write("Conda ENV: {}\n".format(my_env.get("CONDA_DEFAULT_ENV", "N/A")))
+        _ = logfile.write("\n")
+    return
+
+
