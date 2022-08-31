b16a3a22f89dd98e3dfb14f7b8a7b2701a2b306f
Thomas Weber <thomas.weber@embl.de>
2022-08-31 10:10:46 +0200

    Test debug

--- workflow/scripts/utils/install_R_tarball.R
+++ workflow/scripts/utils/install_R_tarball.R
@@ -1,8 +1,11 @@
 package <- snakemake@input[["tarball"]]
 is_package_available <- require(package)
 
-if (!isTRUE(is_strandphaser_available)) {
-
+if (!isTRUE(is_package_available)) {
+    if (!require("BiocManager", quietly = TRUE)) {
+        install.packages("BiocManager")
+    }
+    BiocManager::install("GenomeInfoDbData")
     install.packages(package)
     quit(save = "no")
 }
