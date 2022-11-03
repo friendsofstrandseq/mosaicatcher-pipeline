f7b6ad4bd42aa1afb0c25a0a7629caee60ecf404
Thomas Weber <thomas.weber@embl.de>
2022-11-03 13:25:50 +0100

    Docker update including T2T custom BSgenome

--- workflow/scripts/utils/install_R_tarball.R
+++ workflow/scripts/utils/install_R_tarball.R
@@ -1,4 +1,12 @@
-package <- snakemake@input[["tarball"]]
+is_snakemake_available <- require(snakemake)
+
+if (isTRUE(is_snakemake_available)) {
+    package <- snakemake@input[["tarball"]]
+}   else {
+    args <- commandArgs(trailingOnly = T)
+    package <- args[1]
+}
+
 is_package_available <- require(package)
 
 if (!isTRUE(is_package_available)) {
