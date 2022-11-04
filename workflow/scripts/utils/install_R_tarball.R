a7ac024e78fae84aecad9bfa269f1c8963ef20aa
Thomas Weber <thomas.weber@embl.de>
2022-11-04 11:56:30 +0100

    Version 1.8.3: temp() bug correction

--- workflow/scripts/utils/install_R_tarball.R
+++ workflow/scripts/utils/install_R_tarball.R
@@ -1,11 +1,13 @@
-is_snakemake_available <- require(snakemake)
+# is_snakemake_available <- require(snakemake)
 
-if (isTRUE(is_snakemake_available)) {
-    package <- snakemake@input[["tarball"]]
-}   else {
-    args <- commandArgs(trailingOnly = T)
-    package <- args[1]
-}
+# if (isTRUE(is_snakemake_available)) {
+#     package <- snakemake@input[["tarball"]]
+# }   else {
+#     args <- commandArgs(trailingOnly = T)
+#     package <- args[1]
+# }
+
+package <- snakemake@input[["tarball"]]
 
 is_package_available <- require(package)
 
