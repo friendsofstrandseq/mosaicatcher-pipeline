bacd04a698a1367a1ad35df6de511df1983d4b75
Thomas Weber <thomas.weber@embl.de>
2023-01-06 10:33:20 +0100

    1.8.6: visualization update, conda cleaning, list_commands help, linting correction

--- workflow/scripts/utils/symlink_selected_bam.py
+++ workflow/scripts/utils/symlink_selected_bam.py
@@ -2,15 +2,23 @@ import subprocess
 
 if snakemake.config["use_light_data"] is False:
     subprocess.Popen(
-        "ln -s  {input_bam} {output_bam}".format(input=snakemake.input.bam, file=snakemake.output.bam), shell=True, stdout=subprocess.PIPE
+        "ln -s  {input_bam} {output_bam}".format(input_bam=snakemake.input.bam, output_bam=snakemake.output.bam),
+        shell=True,
+        stdout=subprocess.PIPE,
     )
     subprocess.Popen(
-        "ln -s  {input_bai} {output_bai}".format(input=snakemake.input.bai, file=snakemake.output.bai), shell=True, stdout=subprocess.PIPE
+        "ln -s  {input_bai} {output_bai}".format(input_bai=snakemake.input.bai, output_bai=snakemake.output.bai),
+        shell=True,
+        stdout=subprocess.PIPE,
     )
 else:
     subprocess.Popen(
-        "cp {input_bam} {output_bam}".format(input=snakemake.input.bam, file=snakemake.output.bam), shell=True, stdout=subprocess.PIPE
+        "cp {input_bam} {output_bam}".format(input_bam=snakemake.input.bam, output_bam=snakemake.output.bam),
+        shell=True,
+        stdout=subprocess.PIPE,
     )
     subprocess.Popen(
-        "cp {input_bai} {output_bai}".format(input=snakemake.input.bai, file=snakemake.output.bai), shell=True, stdout=subprocess.PIPE
+        "cp {input_bai} {output_bai}".format(input_bai=snakemake.input.bai, output_bai=snakemake.output.bai),
+        shell=True,
+        stdout=subprocess.PIPE,
     )
