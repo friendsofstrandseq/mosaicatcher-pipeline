f7c9ba140a77b41ebb773b3febc51ef2b7c0f7f9
Thomas Weber <thomas.weber@embl.de>
2023-03-06 14:20:25 +0100

    Version 1.9.2: Major update, scNOVA binding Functional version, optimisations are still required Done: - Docs - Zenodo upload - Conda envs - Path update to be compliant to mosaicatcher standards - Code optimisation (DL parallelisation instead of sequential steps) - deeptools to bedtools + parallelisation followed by aggregation - Prefix path inside R & perl scripts

--- workflow/scripts/strandphaser_scripts/StrandPhaseR_pipeline.R
+++ workflow/scripts/strandphaser_scripts/StrandPhaseR_pipeline.R
@@ -5,15 +5,15 @@ args <- commandArgs(TRUE)
 # add user defined path to load needed libraries
 .libPaths(c(.libPaths(), args[6]))
 
-# suppressPackageStartupMessages(library(StrandPhaseR))
+suppressPackageStartupMessages(library(StrandPhaseR))
 
 # FIXME : tmp debuging local repo
-library(devtools)
+# library(devtools)
 
 # load package w/o installing
 # load_all("/g/korbel2/weber/Gits/StrandPhaseR")
-strandphaser_path <- "/g/korbel2/weber/Gits/Strandphaser_clean/StrandPhaseR"
-print(strandphaser_path)
-load_all(strandphaser_path)
+# strandphaser_path <- "/g/korbel2/weber/Gits/Strandphaser_clean/StrandPhaseR"
+# print(strandphaser_path)
+# load_all(strandphaser_path)
 
 strandPhaseR(inputfolder = args[1], outputfolder = args[2], configfile = args[3], WCregions = args[4], positions = args[5], fillMissAllele = args[5])
