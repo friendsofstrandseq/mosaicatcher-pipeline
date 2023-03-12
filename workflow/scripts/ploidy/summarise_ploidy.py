829185bd540390ad2b5b1fec36d38745b6cb3fbd
Thomas Weber <thomas.weber@embl.de>
2023-03-12 10:04:43 +0100

    Version 2.0.0: Major update, scNOVA binding Versioning harmonisation Fixing BSgenome install issue (trial 2) Fixing dataframe configuration issue related to selected cells to be used in scNOVA Functional version, optimisations are still required Done: - Docs - Zenodo upload - Conda envs - Path update to be compliant to mosaicatcher standards - Code optimisation (DL parallelisation instead of sequential steps) - deeptools to bedtools + parallelisation followed by aggregation - Prefix path inside R & perl scripts

--- workflow/scripts/ploidy/summarise_ploidy.py
+++ workflow/scripts/ploidy/summarise_ploidy.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df = pd.read_csv(snakemake.input.ploidy, sep="\t")
 df = df.groupby("#chrom")["ploidy_estimate"].describe()
+print(df)
 df.to_csv(snakemake.output.summary, sep="\t")
