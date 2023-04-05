72bf5a02e56614ae9fd1ebc0486997dec2a6520a
Thomas Weber <thomas.weber@embl.de>
2023-04-05 14:50:26 +0200

    Version 2.1.1: multistep normalisation for SV calling T2T, hg19 assemblies GC matrices added

--- workflow/scripts/utils/reformat_ms_norm.py
+++ workflow/scripts/utils/reformat_ms_norm.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv(snakemake.input[0], sep=",", compression="gzip")
 df = df[["chrom", "start", "end", "sample", "cell", "c", "w", "class"]]
+df["w"] = df["w"].fillna(0)
+df["c"] = df["c"].fillna(0)
 df["class"] = df["class"].fillna("None")
 df["class"] = df["class"].astype(str)
 df.to_csv(snakemake.output[0], sep="\t", index=False, compression="gzip")
