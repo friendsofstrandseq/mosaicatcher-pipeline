ec910ddc0622caa2eb87a7c2b2c3c88f875ddd47
Thomas Weber <thomas.weber@embl.de>
2023-12-05 10:39:39 +0000

    Minor fixes for scNOVA (2.2.4)

--- workflow/scripts/scNOVA_scripts/filter_input_subclonality.py
+++ workflow/scripts/scNOVA_scripts/filter_input_subclonality.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv(snakemake.input[0], sep="\t")
+df = pd.read_csv(snakemake.input.subclonality, sep="\t")
 df.loc[df["Subclonality"] == snakemake.wildcards.clone].to_csv(
     snakemake.output[0], sep="\t", index=False
 )
